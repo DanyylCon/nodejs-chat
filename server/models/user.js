0185dc85e5b5292c350c9ba29185cbfdb7b88bcb
Danyyl <danyyl.con@gmail.com>
2022-06-07 22:23:37 +0300

    add bcrypt and hash the password in the user model

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const { string } = require('nunjucks/src/filters');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new Schema({
     username: { type: String },
@@ -9,6 +10,11 @@ const UserSchema = new Schema({
 }, {
     versionKey: false,
     collection: 'UsersCollection'
+});
+
+UserSchema.pre('save', function (next) {
+    if ( this.isModified('password') || this.isNew() ) this.password = bcrypt.hashSync(this.password, 12);
+    next();
 });
 
 module.exports = mongoose.model('UserModel', UserSchema);
